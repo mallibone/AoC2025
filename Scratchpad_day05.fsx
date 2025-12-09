3254eaa1f9907aea7eda8dcd5f4606e5644d0f64
Mark Allibone <mallibone@outlook.com>
2025-12-09 10:53:42 +0100

    Solve day 5 part I

--- Scratchpad_day05.fsx
+++ Scratchpad_day05.fsx
@@ -32,11 +32,45 @@ let getInput (day:int) =
 //         | Match "Blueprint (.*): Each ore robot costs (.*) ore. Each clay robot costs (.*) ore. Each obsidian robot costs (.*) ore and (.*) clay. Each geode robot costs (.*) ore and (.*) obsidian." [ blueprintId; oreRobotCost; clayRobotCost; obsidianRobotOreCost; obsidianRobotClayCost; geodeRobotOreCost; geodeRobotObsidianCost; ] ->
 //             {Id = int blueprintId; OreRobotOreCost = int oreRobotCost; ClayRobotOreCost = int clayRobotCost; ObsidianRobotOreCost = int obsidianRobotOreCost; ObsidianRobotClayCost = int obsidianRobotClayCost; GeodeRobotObsidianCost = int geodeRobotObsidianCost; GeodeRobotOreCost = int geodeRobotOreCost}
 
+let parseInput (inputLines: string seq) =
+    let freshIngredientIdRanges = 
+        inputLines 
+        |> Seq.takeWhile (fun line -> not (String.IsNullOrWhiteSpace line))
+        |> Seq.map (fun line -> 
+            let parts = line.Split('-')
+            (int64 parts.[0], int64 parts.[1]))
+        |> Seq.toArray
+
+    
+    let ingredientIds =
+        inputLines
+        |> Seq.skip ((inputLines |> Seq.findIndex (fun line -> String.IsNullOrWhiteSpace line)) + 1)
+        |> Seq.map int64
+        |> Seq.toArray
+
+    freshIngredientIdRanges, ingredientIds
+    
+let findFreshIngredients (freshIngredientIdRanges: (int64 * int64)[], ingredientIds: int64[]) =
+    ingredientIds
+    |> Array.filter (fun id -> 
+        freshIngredientIdRanges 
+        |> Array.exists (fun (startId, endId) -> id >= startId && id <= endId))
+
 // part 1
-// getInput 5
+getInput 5
 // getTestInput 5
+|> parseInput
+|> findFreshIngredients
+|> Array.length
+
 
 // part 2
 // getInput 5
-// getTestInput 5
+getTestInput 5
+|> parseInput
+|> fun (freshIngredientIdRanges, _) -> 
+    freshIngredientIdRanges 
+    |> Array.collect (fun (startId, endId) -> Array.init (int (endId - startId + 1L)) (fun i -> startId + int64 i))
+    |> HashSet
+
 
