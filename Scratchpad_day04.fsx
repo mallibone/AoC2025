13c739447d8424f4806aa30157cd1d6cea6f4288
Mark Allibone <mallibone@outlook.com>
2025-12-09 07:19:00 +0100

    Solve day 4 part I

--- Scratchpad_day04.fsx
+++ Scratchpad_day04.fsx
@@ -32,9 +32,48 @@ let getInput (day:int) =
 //         | Match "Blueprint (.*): Each ore robot costs (.*) ore. Each clay robot costs (.*) ore. Each obsidian robot costs (.*) ore and (.*) clay. Each geode robot costs (.*) ore and (.*) obsidian." [ blueprintId; oreRobotCost; clayRobotCost; obsidianRobotOreCost; obsidianRobotClayCost; geodeRobotOreCost; geodeRobotObsidianCost; ] ->
 //             {Id = int blueprintId; OreRobotOreCost = int oreRobotCost; ClayRobotOreCost = int clayRobotCost; ObsidianRobotOreCost = int obsidianRobotOreCost; ObsidianRobotClayCost = int obsidianRobotClayCost; GeodeRobotObsidianCost = int geodeRobotObsidianCost; GeodeRobotOreCost = int geodeRobotOreCost}
 
+let parseToMap (input: string) =
+    input.ToCharArray()
+    |> Array.map string
+
+let neighborCount (map: string array array) (x:int) (y:int) =
+    let rows = map.Length
+    let cols = map.[0].Length
+
+    let directions = [| (-1, 0); (-1, -1); (-1, 1); (1, 0); (1, -1); (1, 1); (0, -1); (0, 1) |]
+
+    let validNeighcoords = 
+        directions 
+        |> Array.map (fun (dx, dy) -> (x + dx, y + dy))
+        |> Array.filter (fun (newX, newY) -> newX >= 0 && newX < rows && newY >= 0 && newY < cols)
+    
+    let foundNeighbours = 
+        validNeighcoords 
+        |> Array.filter (fun (newX, newY) -> map.[newX].[newY] = "@")
+        |> Array.length
+
+    // printfn "%A %A %A %A" foundNeighbours x y validNeighcoords
+
+    foundNeighbours
+
+let findPaperRollsThatCanBeMoved (map: string array array) =
+    let rows = map.Length
+    let cols = map.[0].Length
+
+    let mutable count = 0
+
+    for x in 0 .. rows - 1 do
+        for y in 0 .. cols - 1 do
+            if map[x][y] = "@" && neighborCount map x y < 4 then
+                count <- count + 1
+
+    count
+
 // part 1
 // getInput 4
-// getTestInput 4
+getTestInput 4
+|> Array.map parseToMap
+|> findPaperRollsThatCanBeMoved
 
 // part 2
 // getInput 4
