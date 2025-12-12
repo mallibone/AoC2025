091692d8fee28f130f8c41b273cd9172fe4e7e25
Mark Allibone <mallibone@outlook.com>
2025-12-12 16:26:47 +0100

    Solves day 9 part I

--- Scratchpad_day09.fsx
+++ Scratchpad_day09.fsx
@@ -32,9 +32,37 @@ let getInput (day:int) =
 //         | Match "Blueprint (.*): Each ore robot costs (.*) ore. Each clay robot costs (.*) ore. Each obsidian robot costs (.*) ore and (.*) clay. Each geode robot costs (.*) ore and (.*) obsidian." [ blueprintId; oreRobotCost; clayRobotCost; obsidianRobotOreCost; obsidianRobotClayCost; geodeRobotOreCost; geodeRobotObsidianCost; ] ->
 //             {Id = int blueprintId; OreRobotOreCost = int oreRobotCost; ClayRobotOreCost = int clayRobotCost; ObsidianRobotOreCost = int obsidianRobotOreCost; ObsidianRobotClayCost = int obsidianRobotClayCost; GeodeRobotObsidianCost = int geodeRobotObsidianCost; GeodeRobotOreCost = int geodeRobotOreCost}
 
+let parseInput (line:string) =
+    line.Split(',')
+    |> Array.map int64
+
+let findFurthestEdges (inputs:int64[][]) =
+    // calculate distance between each point (2d)
+    let distances =
+        [|
+            for i in 0 .. inputs.Length - 1 do
+                for j in i + 1 .. inputs.Length - 1 do
+                    let p1 = inputs[i]
+                    let p2 = inputs[j]
+                    let dist =
+                        Math.Sqrt(
+                            p1
+                            |> Array.zip p2
+                            |> Array.sumBy (fun (a, b) -> float ((a - b) * (a - b)))
+                        )
+                    yield (dist, (p1, p2))
+        |]
+    distances
+
 // part 1
-// getInput 9
+getInput 9
 // getTestInput 9
+|> Array.map parseInput
+|> findFurthestEdges
+|> Array.maxBy fst
+|> fun (_, (p1, p2)) ->
+    printfn "%A %A" (Math.Max(p1[0],p2[0]) - Math.Min(p1[0],p2[0]) + 1L) (Math.Max(p1[1], p2[1]) - Math.Min(p1[1], p2[1]) + 1L)
+    (Math.Max(p1[0],p2[0]) - Math.Min(p1[0],p2[0]) + 1L) * (Math.Max(p1[1], p2[1]) - Math.Min(p1[1], p2[1]) + 1L)
 
 // part 2
 // getInput 9
